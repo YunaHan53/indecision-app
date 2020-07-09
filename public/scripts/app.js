f8d393e797ec5392ead2d1ef1267c70d0f29b3b4
YunaH <YunaH503@gmail.com>
2020-07-09 18:36:57 -0400

    Practice JSX expressions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,74 @@
-"use strict";
+'use strict';
 
 console.log("App.js is running!");
 
+var app = {
+  title: 'Indecision App',
+  subtitle: 'Put your life in the hands of a computer'
+};
+
 // JSX - JavaScript XML
 var template = React.createElement(
-  "h1",
+  'div',
   null,
-  "Indecision App"
+  React.createElement(
+    'h1',
+    null,
+    app.title
+  ),
+  React.createElement(
+    'p',
+    null,
+    app.subtitle
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item one'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item two'
+    )
+  )
 );
+
+// User template
+var user = {
+  name: 'Mike',
+  age: 27,
+  location: 'Los Angeles'
+
+  // var userName = 'Mike'
+  // var userAge = '27'
+  // var userLocation = 'Los Angeles'
+
+};var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    user.name.toUpperCase() + '!'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Age: ',
+    user.age
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Location: ',
+    user.location
+  )
+);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,7 +1,42 @@
 console.log("App.js is running!")
 
+var app = {
+  title: 'Indecision App',
+  subtitle: 'Put your life in the hands of a computer'
+};
+
 // JSX - JavaScript XML
-var template = <h1>Indecision App</h1>;
+var template = (
+  <div>
+    <h1>{app.title}</h1>
+    <p>{app.subtitle}</p>
+    <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+    </ol>
+  </div>
+);
+
+
+// User template
+var user = {
+  name: 'Mike',
+  age: 27,
+  location: 'Los Angeles'
+}
+
+// var userName = 'Mike'
+// var userAge = '27'
+// var userLocation = 'Los Angeles'
+
+var templateTwo = (
+  <div>
+    <h1>{user.name.toUpperCase() + '!'}</h1>
+    <p>Age: {user.age}</p>
+    <p>Location: {user.location}</p>
+  </div>
+)
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
