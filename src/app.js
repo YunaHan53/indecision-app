88fb37453945845633b891910bbc46437d590a73
YunaH <YunaH503@gmail.com>
2020-08-04 23:01:22 -0400

    Added nested component inside Options

--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
+class IndecisionApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Action />
+        <Options />
+        <AddOption />
+      </div>
+    )
+  }
+}
+
 class Header extends React.Component {
   render() {
     return (
@@ -25,6 +38,18 @@ class Options extends React.Component {
     return (
       <div>
         Options
+        <Option />
+      </div>
+    )
+  }
+}
+
+// Option => Option component here
+class Option extends React.Component {
+  render() {
+    return (
+      <div>
+        Option
       </div>
     )
   }
@@ -41,13 +66,5 @@ class AddOption extends React.Component {
   }
 }
 
-const jsx = (
-  <div>
-    <Header />
-    <Action />
-    <Options />
-    <AddOption />
-  </div>
-)
 
-ReactDOM.render(jsx, document.getElementById('app'))
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
